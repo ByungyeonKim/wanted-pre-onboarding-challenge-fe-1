f0727fe96a4507f33560e817f3c686b7b932da1a
ByungyeonKim <viva_kby@naver.com>
2023-01-15 00:03:57 +0900

    :lipstick: 이미지 효과 애니메이션 추가

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'fade-in-image': 'fade-in 550ms ease-out 300ms forwards',
+      },
+      keyframes: {
+        'fade-in': {
+          '0%': { transform: 'scale(1.2)' },
+          '100%': { opacity: '1', transform: 'scale(1)' },
+        },
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 };
